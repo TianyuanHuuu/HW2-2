7594a3d027afd1f568ece92302dc761cbaf1b709
TianyuanHuuu <160672802+TianyuanHuuu@users.noreply.github.com>
2025-03-24 21:51:16 -0400

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -6,30 +6,41 @@ import json
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.to_checksum_address(bayc_address)
 
-# You will need the ABI to connect to the contract
-# The file 'abi.json' has the ABI for the bored ape contract
-# In general, you can get contract ABIs from etherscan
-# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+# Load ABI
 with open('ape_abi.json', 'r') as f:
     abi = json.load(f)
 
-############################
-# Connect to an Ethereum node
-api_url = ""  # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
+api_url = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"  # ← insert your actual project ID
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
+# Create contract instance
+contract = web3.eth.contract(address=contract_address, abi=abi)
 
 def get_ape_info(ape_id):
     assert isinstance(ape_id, int), f"{ape_id} is not an int"
-    assert 0 <= ape_id, f"{ape_id} must be at least 0"
-    assert 9999 >= ape_id, f"{ape_id} must be less than 10,000"
+    assert 0 <= ape_id <= 9999, f"{ape_id} must be between 0 and 9,999"
 
     data = {'owner': "", 'image': "", 'eyes': ""}
 
-    # YOUR CODE HERE
+    owner = contract.functions.ownerOf(ape_id).call()
 
-    assert isinstance(data, dict), f'get_ape_info{ape_id} should return a dict'
-    assert all([a in data.keys() for a in
-                ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
+    token_uri = contract.functions.tokenURI(ape_id).call()
+
+    ipfs_hash = token_uri.replace("ipfs://", "")
+    metadata_url = f"https://ipfs.io/ipfs/{ipfs_hash}"
+
+    response = requests.get(metadata_url)
+    metadata = response.json()
+
+    image = metadata.get("image", "")
+    attributes = metadata.get("attributes", [])
+    eyes = next((trait["value"] for trait in attributes if trait["trait_type"] == "Eyes"), "")
+
+    data['owner'] = owner
+    data['image'] = image
+    data['eyes'] = eyes
+
+    assert isinstance(data, dict), f'get_ape_info({ape_id}) should return a dict'
+    assert all(k in data for k in ['owner', 'image', 'eyes']), f"Return value must include keys: owner, image, eyes"
     return data
